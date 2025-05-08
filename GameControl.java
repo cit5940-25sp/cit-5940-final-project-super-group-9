856286a97f7bc4af591e1f16d58bbafde3f67cfb
jia-babe <ruijial@seas.upenn.edu>
2025-05-08 00:35:48 -0400

    add GameControl class

--- GameControl.java
+++ GameControl.java
@@ -1,9 +1,21 @@
 
 
 public class GameControl extends Control{
-    private SimpleIO io;
+    private SimpleIO simpleIO;
     public GameControl(){
-        io = new SimpleIO();
+        simpleIO = new SimpleIO();
     }
+    public void runInput(){
+        GameModel gameModel = (GameModel) getModel();
+        while (true) {
+            String title = simpleIO.getInput(getModel());
 
+            if (title != null && !gameModel.getMovies().contains(title)){
+                continue;
+            }else{
+                gameModel.inputMovie(title);
+                break;
+            }
+        }
+    }
 }

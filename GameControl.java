62603114dedccd6fc8da240dbe4456a42623834b
jia-babe <ruijial@seas.upenn.edu>
2025-05-09 02:34:38 -0400

    GameControl Version 2

--- GameControl.java
+++ GameControl.java
@@ -1,21 +1,32 @@
+import java.io.IOException;
 
-
+/**
+ * GameControl class extends the base Control class, acting as a controller in the MVC architecture.
+ * It is responsible for handling user input using the SimpleIO class.
+ */
 public class GameControl extends Control{
-    private SimpleIO simpleIO;
-    public GameControl(){
-        simpleIO = new SimpleIO();
-    }
-    public void runInput(){
-        GameModel gameModel = (GameModel) getModel();
-        while (true) {
-            String title = simpleIO.getInput(getModel());
 
-            if (title != null && !gameModel.getMovies().contains(title)){
-                continue;
-            }else{
-                gameModel.inputMovie(title);
-                break;
-            }
+    public GameControl(){
+    }
+
+    /**
+     * Initiates the input handling process.
+     * Calls the runInput method of the SimpleIO instance with the model retrieved from the base Control class.
+     */
+    public void runInput(){
+        // Invoke the runInput method of SimpleIO with the model
+        SimpleIO simpleIO = new SimpleIO();
+        simpleIO.runInput(getModel());
+        // press any key to continue...
+        /* 
+        System.out.println("Press any key to continue...");
+        try {
+            int inputByte = System.in.read();
+            //System.out.println(inputByte);
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
         }
+        */
+
     }
 }
